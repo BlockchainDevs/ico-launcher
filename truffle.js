980ddfb0dfe1b5026a5f35d2edf37ef99279081a
sambhav2612 <sambhavjain2612@gmail.com>
2018-04-26 11:49:04 +0530

    update backend

--- truffle.js
+++ truffle.js
@@ -1,13 +1,23 @@
 module.exports = {
+  rpc: {
+    host:"localhost",
+    port:8545
+  },
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 4712388     // default
+      gas: 4712388,    // default
+      from: "0000000000000000000000000000000000000001"
     },
     rinkeby: {
-
+      host: "localhost",
+      port: 8545,
+      network_id: "4",
+      gas: 4712388,
+      gasPrice: 100000000000,
+      from: "79a1b6fecfa9c954803d2b59773fadc39d063e64"
     }
   }
 };
